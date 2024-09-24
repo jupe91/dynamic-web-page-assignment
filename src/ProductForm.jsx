6801d943aebcea28c264d9d5dda090ad68a5e7d8
jupe01 <juntunenreetta@gmail.com>
2024-09-24 21:12:55 +0300

    Added product dropdown -used varible 'productList' which contains objects with names and prices

--- src/ProductForm.jsx
+++ src/ProductForm.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class ProductForm extends React.Component {
+    
     render() {
+    const productsList = [
+        {productName: 'tv', productPrice: 789},
+        {productName: 'telephone', productPrice: 240},
+        {productName: 'computer', productPrice: 1000}
+    ];
+    
+
         return (
-            <h3>MMM</h3>
+            <>
+            <h3>Select product</h3>
+            <label>Product: </label>
+            <select>
+                {productsList.map( product => (
+                    <option value={product.productName}>
+                        {product.productName} ({product.productPrice}€)
+                    </option>
+                ))}    
+            </select>
+            </>
         );
     }
 }
