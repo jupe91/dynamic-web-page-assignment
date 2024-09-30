01163a03d85f9c9e4e99b711bba47adbba70eb6d
jupe01 <juntunenreetta@gmail.com>
2024-09-30 18:40:23 +0300

    Added quantity to order form
    -import for useState
    -state handling for quantity
    -buttons for + and -

--- src/ProductForm.jsx
+++ src/ProductForm.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useState } from "react";
 
 const ProductForm = () => {
     const productsList = [
         { productName: 'tv', productPrice: 789 },
         { productName: 'telephone', productPrice: 240 },
         { productName: 'computer', productPrice: 1000 }
-    ];
+    ];  
+    
+    const [quantity, setQuantity] = useState(0);
 
     return (
         <>
@@ -17,7 +20,11 @@ const ProductForm = () => {
                         {product.productName} ({product.productPrice}€) {/* Shows on UI */}
                     </option>
                 ))}
-            </select>
+            </select><br></br>
+            <label>Quantity: </label>
+            <button onClick={() => setQuantity(quantity - 1)}>-</button>
+            <span>{quantity}</span>
+            <button onClick={() => setQuantity(quantity + 1)}>+</button>
         </>
     );
 }
