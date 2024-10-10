85d8094c234f3aa6aded29f23e8041e50c309ea8
jupe01 <juntunenreetta@gmail.com>
2024-10-10 19:54:44 +0300

    Added integration to random-data-api in open data component
    - Fetching random beer data from random-data-api's beers endpoint
    - Displaying fetched beer data
    - Button for refreshing search results

--- src/OpenData.jsx
+++ src/OpenData.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const OpenData = () => {
 
+    const url = 'https://random-data-api.com/api/v2/beers';
+    
+    // State variables to hold beer data
+    const [beerData, setBeerData] = useState(null);
+
+    // Function to fetch beer data
+    const getData = async () => {
+            const response = await fetch(url);
+            const json = await response.json();
+            setBeerData(json);
+    };
+
+    // Fetch beer data when component loads
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div>
-            <h1>Moi</h1>
+            <h1>Random Beer</h1>
+                <div>
+                    <p><strong>Brand:</strong> {beerData.brand}</p>
+                    <p><strong>Name:</strong> {beerData.name}</p>
+                    <p><strong>Style:</strong> {beerData.style}</p>
+                    <p><strong>Malts:</strong> {beerData.malts}</p>
+                    <p><strong>Alcohol Content:</strong> {beerData.alcohol}%</p>
+                </div>
+            <button onClick={getData}>Get Another Beer</button>
         </div>
     );
 }
