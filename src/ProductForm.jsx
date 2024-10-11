11b657ad4428b40f2a922293c50497d303385471
jupe01 <juntunenreetta@gmail.com>
2024-10-11 11:40:27 +0300

    Added comments to ProductForm.jsx

--- src/ProductForm.jsx
+++ src/ProductForm.jsx
@@ -3,18 +3,21 @@ import { useState } from "react";
 import OrderInfo from './OrderInfo';
 
 const ProductForm = () => {
+    //List of products with their name and price
     const productsList = [
         { productName: 'tv', productPrice: 789 },
         { productName: 'telephone', productPrice: 240 },
         { productName: 'computer', productPrice: 1000 }
     ];  
     
+    // State handling for selected product and quantity
     const [selectedProduct, setSelectedProduct] = useState(productsList[0]);
     const [quantity, setQuantity] = useState(0);
 
     return (
         <>
             <h3>Select product</h3>
+            {/* Dropdown to select a product, updating the selectedProduct state based on user selection */}
             <label>Product: </label>
             <select onChange={(e) => {
                 const product = productsList.find(p => p.productName === e.target.value);
@@ -22,14 +25,16 @@ const ProductForm = () => {
             }}>
                 {productsList.map(product => (
                     <option key={product.productName} value={product.productName}>
-                        {product.productName} ({product.productPrice}€) {/* Shows on UI */}
+                        {product.productName} ({product.productPrice}€) {/* Shows product name and price */}
                     </option>
                 ))}
             </select><br></br>
+            {/*Quantity control*/}
             <label>Quantity: </label>
             <button onClick={() => setQuantity(Math.max(quantity  - 1, 0))}>-</button>
             <span>{quantity}</span>
             <button onClick={() => setQuantity(quantity + 1)}>+</button>
+            {/*Shows order information*/}
             <OrderInfo productName={selectedProduct.productName}
             productPrice={selectedProduct.productPrice}
             quantity={quantity}></OrderInfo>
